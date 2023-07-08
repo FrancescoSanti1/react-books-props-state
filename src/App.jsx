01ebdfd68d47641b19313350a7ee354437831968
Francesco Santi <88621180+FrancescoSanti1@users.noreply.github.com>
2023-07-08 19:28:12 +0200

    receive new book title on form submit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+
+export default function App() {
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log("Need to add book with title:", title);
+  }
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
+}
