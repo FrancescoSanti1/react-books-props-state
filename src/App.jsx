7651f5524491a0c7eeb4d865d5465ec7071b7a76
Francesco Santi <88621180+FrancescoSanti1@users.noreply.github.com>
2023-07-15 12:37:32 +0200

    delete a book using api

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,9 @@ export default function App() {
     setBooks([...books, response.data]);
   }
 
-  const deleteBook = (id) => {
+  const deleteBook = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
     setBooks(books.filter((book) => book.id !== id));
   }
 
